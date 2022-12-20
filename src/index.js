e9340483f470b3ec0ea90762fda9d250ca7fb880
GraysTania <101881748+GraysTania@users.noreply.github.com>
2022-12-20 22:35:54 +0200

    111

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import { fetchCountries } from './js/fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+
+const refInput = document.querySelector('#search-box');
+const refInfo = document.querySelector('.country-info');
+const refList = document.querySelector('.country-list');
+
+refInput.addEventListener(
+  'input',
+  debounce(event => {
+    let countryInput = event.target.value.trim();
+    refList.innerHTML = '';
+    refInfo.innerHTML = '';
+    if (countryInput === '') {
+      return;
+    }
+    fetchCountries(countryInput);
+  }, DEBOUNCE_DELAY)
+);
